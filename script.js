ceb0dc972217ab3ae8835f22d5ddeecdbdbb59ec
Tyree <tyreewhite01102006@gmail.com>
2023-06-04 13:00:36 -0500

    Change info corresponding to player turn

--- script.js
+++ script.js
@@ -16,12 +16,21 @@ const gameCells = document.querySelectorAll('.cell');
 let marker;
 gameCells.forEach(cell => cell.addEventListener('click', function placeMarker() {
     cell.removeEventListener('click', placeMarker);
+    const gameInfo = document.querySelector('.gameInfo');
     if (marker === undefined) {
         marker = playerOne.marker;
     } else if (marker === playerOne.marker) {
         marker = playerTwo.marker;
     } else if (marker === playerTwo.marker) {
         marker = playerOne.marker;
+    }
+
+    if (gameInfo.textContent === 'X Goes First') {
+        gameInfo.textContent = "Player O's Turn";
+    } else if (gameInfo.textContent === "Player O's Turn") {
+        gameInfo.textContent = "Player X's Turn"
+    } else if  (gameInfo.textContent === "Player X's Turn") {
+        gameInfo.textContent = "Player O's Turn"
     }
     cell.textContent = marker;
 }))
